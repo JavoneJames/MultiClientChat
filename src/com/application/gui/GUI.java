a4f440867c5bc2c5258e62a228e94ad9e77fe18a
Jovane <jovanekjames@icloud.com>
2021-10-13 19:48:49 +0100

    created an abstract class that will be the base class for gui implementation for the client and server ui

---
+++ src/com/application/gui/GUI.java
@@ -0,0 +1,10 @@
+package com.application.gui;
+
+public abstract class GUI {
+
+    protected abstract void initComponents();
+    protected abstract void createWindowFrame();
+    protected abstract void attachComponentsToFrame();
+    //protected abstract void handlerForClosingFrame();
+
+}
