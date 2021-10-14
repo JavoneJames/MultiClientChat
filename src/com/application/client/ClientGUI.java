07af1243352240e1e7fd27a615c94940a7fac50e
Jovane <jovanekjames@icloud.com>
2021-10-14 16:35:32 +0100

    refactored to remove duplicate code

--- src/com/application/client/ClientGUI.java
+++ src/com/application/client/ClientGUI.java
@@ -2,77 +2,48 @@ package com.application.client;
 
 import com.application.gui.GUI;
 
-import javax.swing.*;
-import java.awt.*;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class ClientGUI extends GUI {
-    private JFrame frame;
-    private JPanel panel;
-    private JTextField inputTextField;
-    private JButton submitButton;
-    private JTextArea displayServerFeed;
-    private JScrollPane scrollPane;
-    @Override
-    protected void instantiateComponents() {
-        frame = new JFrame();
-        panel = new JPanel();
-        inputTextField = new JTextField();
-        submitButton = new JButton();
-        displayServerFeed = new JTextArea(20,30);
-        scrollPane = new JScrollPane();
-    }
-
-    @Override
-    protected void createWindowFrame() {
-        //calls method that is used to instantiate the components to be used
-        instantiateComponents();
-
-        frame.setTitle("Chat Client");
-        frame.setSize(350,350);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        frame.setResizable(false);
-        attachComponentsToFrame();
-        frame.pack();
-        frame.setVisible(true);
-    }
 
     @Override
     protected void attachComponentsToFrame() {
-        panel.setLayout(new GridBagLayout());
+        super.panel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
-
         //textarea to displayed server feed messages
-        displayServerFeed.setRows(20);
-        displayServerFeed.setColumns(30);
-        scrollPane.setViewportView(displayServerFeed);
-        scrollPane.setWheelScrollingEnabled(true);
-        displayServerFeed.setEditable(false);
-        displayServerFeed.setText(null);
+        super.displayServerFeed.setRows(20);
+        super.displayServerFeed.setColumns(30);
+        super.scrollPane.setViewportView(super.displayServerFeed);
+        super.scrollPane.setWheelScrollingEnabled(true);
+        super.displayServerFeed.setEditable(false);
+        super.displayServerFeed.setText(null);
         gbc.gridwidth = 3;
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.insets = new Insets(10,10,10,10);
         gbc.fill = GridBagConstraints.BOTH;
-        panel.add(displayServerFeed, gbc);
+        super.panel.add(super.displayServerFeed, gbc);
         //text field to input IP the server should try to connect to
-        inputTextField.setColumns(30);
+        super.inputTextField.setColumns(30);
         gbc.gridwidth = 2;
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.insets = new Insets(0,10,0,0);
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(inputTextField, gbc);
+        super.panel.add(super.inputTextField, gbc);
         //submit button for text field data
-        submitButton.setText("Send Message");
+        super.submitButton.setText("Send Message");
         gbc.gridwidth = 1;
         gbc.gridx = 2;
         gbc.gridy = 2;
         gbc.insets = new Insets(5,0,10,10);
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(submitButton, gbc);
+        super.panel.add(super.submitButton, gbc);
 
-        frame.add(panel, null);
+        super.frame.add(super.panel, null);
     }
 }
--- src/com/application/gui/GUI.java
+++ src/com/application/gui/GUI.java
@@ -1,9 +1,38 @@
 package com.application.gui;
 
-public abstract class GUI {
+import javax.swing.*;
 
-    protected abstract void instantiateComponents();
-    protected abstract void createWindowFrame();
+public abstract class GUI {
+    protected JFrame frame;
+    protected JPanel panel;
+    protected javax.swing.JTextField inputTextField;
+    protected JButton submitButton;
+    protected JTextArea displayServerFeed;
+    protected JScrollPane scrollPane;
+    protected JLabel displayLabel;
+
+    protected void instantiateComponents(){
+        frame = new JFrame();
+        panel = new JPanel();
+        inputTextField = new JTextField();
+        submitButton = new JButton();
+        displayLabel = new JLabel();
+        displayServerFeed = new JTextArea();
+        scrollPane = new JScrollPane();
+    }
+    public void createWindowFrame(){
+        //calls method that is used to instantiate the components to be used
+        instantiateComponents();
+
+        frame.setTitle("Chat Client");
+        frame.setSize(350,350);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setResizable(false);
+        attachComponentsToFrame();
+        frame.pack();
+        frame.setVisible(true);
+    }
     protected abstract void attachComponentsToFrame();
     //protected abstract void handlerForClosingFrame();
 
