f8b709104bd3a1547eb1c76d37b67da50c026a41
Jovane <jovanekjames@icloud.com>
2021-10-15 10:53:25 +0100

    instantiated a serversocket object and pass the 'port' variable through its constructor - also refactored text field validation to another method

--- src/com/application/server/ServerProcess.java
+++ src/com/application/server/ServerProcess.java
@@ -1,6 +1,9 @@
 package com.application.server;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.rmi.server.ServerNotActiveException;
 
 public class ServerProcess extends ServerGUI implements Runnable {
 
@@ -12,12 +15,22 @@ public class ServerProcess extends ServerGUI implements Runnable {
     public void run() {
         submitButton.addActionListener(e -> {
             if (e.getSource() == submitButton)
-                if (inputTextField.getText().isEmpty())//checks if the text field is empty
-                    JOptionPane.showMessageDialog(frame, "Empty input");
-                else if (inputTextField.getText().matches("^[a-zA-Z]+$") ||//checks if input is string
-                        !(Short.parseShort(inputTextField.getText()) >= 1))//checks if input is a positive int
-                    JOptionPane.showMessageDialog(frame, "Enter positive Integer");
-
+                textFieldInputHandler();
+            short PORT = Short.parseShort(inputTextField.getText());
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                if (serverSocket.isClosed())
+                    throw new ServerNotActiveException(PORT + " cannot be accessed");
+            } catch (IOException | ServerNotActiveException ioException) {
+                ioException.printStackTrace();
+            }
         });
+    }
+
+    private void textFieldInputHandler() {
+        if (inputTextField.getText().isEmpty())//checks if the text field is empty
+            JOptionPane.showMessageDialog(frame, "Empty input");
+        else if (inputTextField.getText().matches("^[a-zA-Z]+$") ||//checks if input is string
+                !(Short.parseShort(inputTextField.getText()) >= 1))//checks if input is a positive int
+            JOptionPane.showMessageDialog(frame, "Enter positive Integer");
     }
 }
