cfa0ac4c03ce1bd50b6b995f8857915ac5ce241e
Jovane <jovanekjames@icloud.com>
2021-10-15 10:34:27 +0100

    implemented validation checking for the server client text field

--- src/com/application/server/ServerProcess.java
+++ src/com/application/server/ServerProcess.java
@@ -1,17 +1,23 @@
 package com.application.server;
 
-public class ServerProcess extends ServerGUI implements Runnable{
+import javax.swing.*;
 
-    ServerProcess(){
+public class ServerProcess extends ServerGUI implements Runnable {
+
+    ServerProcess() {
         createWindowFrame();
     }
 
     @Override
     public void run() {
-        submitButton.addActionListener(e ->{
-            if (e.getSource() == super.submitButton)
-               if (super.inputTextField.getText().isEmpty())
-                   System.out.println("empty");
+        submitButton.addActionListener(e -> {
+            if (e.getSource() == submitButton)
+                if (inputTextField.getText().isEmpty())//checks if the text field is empty
+                    JOptionPane.showMessageDialog(frame, "Empty input");
+                else if (inputTextField.getText().matches("^[a-zA-Z]+$") ||//checks if input is string
+                        !(Short.parseShort(inputTextField.getText()) >= 1))//checks if input is a positive int
+                    JOptionPane.showMessageDialog(frame, "Enter positive Integer");
+
         });
     }
 }
