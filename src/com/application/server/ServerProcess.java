77bf1ee300f1d6428fb4f11d98b584ac1bf62e89
Jovane <jovanekjames@icloud.com>
2021-10-15 11:46:12 +0100

    added implementation to accept multiple client request through serverSocket obj to Socket obj

--- src/com/application/server/ServerProcess.java
+++ src/com/application/server/ServerProcess.java
@@ -1,11 +1,16 @@
 package com.application.server;
 
-import javax.swing.*;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.JOptionPane;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.rmi.server.ServerNotActiveException;
 
 public class ServerProcess extends ServerGUI implements Runnable {
+
 
     ServerProcess() {
         createWindowFrame();
@@ -17,13 +22,29 @@ public class ServerProcess extends ServerGUI implements Runnable {
             if (e.getSource() == submitButton)
                 textFieldInputHandler();
             short PORT = Short.parseShort(inputTextField.getText());
-            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                if (serverSocket.isClosed())
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {//instantiate serverSocket object - param 'PORT'
+                if (serverSocket.isClosed() || serverSocket.isBound())//checks is the serverSocket is available
                     throw new ServerNotActiveException(PORT + " cannot be accessed");
+                displayServerFeed.append("Starting Server...");
+                incomingConnectionsHandler(serverSocket);
             } catch (IOException | ServerNotActiveException ioException) {
                 ioException.printStackTrace();
             }
         });
+    }
+    @SuppressWarnings("InfiniteLoopStatement")
+    private void incomingConnectionsHandler(@NotNull ServerSocket serverSocket){
+        while (true){
+            Socket socket;
+            try {
+                socket = serverSocket.accept();
+                if (socket.isClosed())
+                    throw new SocketException("cannot connect to client socket");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     private void textFieldInputHandler() {
