37d57b35f6c5e8fdfb1f69bc7f04fcd7249c0108
Jovane <jovanekjames@icloud.com>
2021-11-01 19:04:37 +0000

    created user object - each client gets an ID

---
+++ Server/src/main/java/com/server/User.java
@@ -0,0 +1,13 @@
+package com.server;
+
+public class User {
+  private final int ID;
+
+  public User(int id) {
+    ID = id;
+  }
+
+  public int getID() {
+    return ID;
+  }
+}
--- Server/src/main/java/com/server/process/ServerProcess.java
+++ Server/src/main/java/com/server/process/ServerProcess.java
@@ -2,9 +2,7 @@ package com.server.process;
 
 import com.server.gui.ServerGUI;
 
-import javax.swing.SwingUtilities;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
@@ -13,47 +11,51 @@ import java.util.concurrent.Executors;
 
 public class ServerProcess extends ServerGUI {
 
-    private ServerSocket serverSocket;
-    Vector<ClientHandler> clientHandlerVector = new Vector<>();
+  Vector<ClientHandler> clientHandlerVector = new Vector<>();
+  private ServerSocket serverSocket;
+  private static int counter = 1;
 
-    ServerProcess(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
-            if (serverSocket.isClosed())
-                throw new RuntimeException("cannot connect to port: " + port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  ServerProcess(int port) {
+    try {
+      serverSocket = new ServerSocket(port);
+      if (serverSocket.isClosed())
+        throw new RuntimeException("cannot connect to port: " + port);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    protected void serverSocketHandler() {
-        System.out.println("starting....");
-        displayServerFeed.append("starting...\n");
-            new Thread(() -> incomingConnectionsHandler(serverSocket)).start();
-    }
+  @Override
+  protected void serverSocketHandler() {
+    System.out.println("starting....");
+    displayServerFeed.append("starting...\n");
+    new Thread(() -> incomingConnectionsHandler(serverSocket)).start();
+  }
 
-    private void incomingConnectionsHandler(ServerSocket serverSocket) {
-        while (true) {
-            System.out.println("waiting...");
-            displayServerFeed.append("waiting...\n");
-            try {
-                Socket socket = serverSocket.accept();
-                System.out.println("connection accepted...");
-                displayServerFeed.append("connection accepted...\n");
-                multiClientHandler(socket);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+  private void incomingConnectionsHandler(ServerSocket serverSocket) {
+    while (true) {
+      System.out.println("waiting...");
+      displayServerFeed.append("waiting...\n");
+      try {
+        Socket socket = serverSocket.accept();
+        System.out.println("connection accepted...");
+        displayServerFeed.append("connection accepted...\n");
+        multiClientHandler(socket);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    private void multiClientHandler(Socket socket) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        ClientHandler clientHandler = new ClientHandler(socket);
-        System.out.println("here method");
-        executorService.execute(clientHandler::execute);
-        clientHandlerVector.addElement(clientHandler);
-        System.out.println("created method");
-    }
+  private void multiClientHandler(Socket socket) {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    ClientHandler clientHandler = new ClientHandler(socket);
+    System.out.println("here method");
+    executorService.execute(clientHandler::execute);
+    clientHandlerVector.addElement(clientHandler);
+    System.out.println("created method");
+  }
+
+    int getCounter(){return counter++;}
+
 }
